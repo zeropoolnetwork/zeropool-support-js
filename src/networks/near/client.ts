6ffce61cfcaa105dae43298b47c54d4922b594a1
Dmitry Vdovin <voidxnull@gmail.com>
2022-10-06 21:51:07 +0400

    Implement sign for near

--- src/networks/near/client.ts
+++ src/networks/near/client.ts
@@ -6,6 +6,7 @@ import { InMemoryKeyStore } from 'near-api-js/lib/key_stores';
 import { JsonRpcProvider } from 'near-api-js/lib/providers';
 import { connect, ConnectConfig, Contract, Near } from 'near-api-js';
 import { parseSeedPhrase } from 'near-seed-phrase';
+import { Buffer } from 'buffer';
 
 import { Client } from '../client';
 import { TxFee } from '../transaction';
@@ -107,5 +108,14 @@ export class NearClient extends Client {
       gasPrice: gasPrice.toString(),
       fee: feeFormatted,
     };
+  }
+
+  public async sign(data: string): Promise<string> {
+    if (data.slice(0, 2) == '0x') {
+      data = data.slice(2);
+    }
+    const dataArray = new Uint8Array(Buffer.from(data, 'hex'));
+    const sign = await this.account.connection.signer.signMessage(dataArray, this.account.accountId, this.config.networkId);
+    return Buffer.from(sign.signature).toString('hex');
   }
 }
