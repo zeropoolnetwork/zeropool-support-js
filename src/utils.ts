c60a291f756e204d8a40ca0315f96660292396ff
Dmitry Vdovin <voidxnull@gmail.com>
2023-10-19 13:21:46 +0400

    Fix toBaseUnit for amount format "0.*"

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,11 @@ export function toBaseUnit(amount: string, decimals: number): string {
   if (parts.length === 1) {
     return parts[0] + '0'.repeat(decimals);
   } else if (parts.length === 2) {
-    return parts[0] + parts[1] + '0'.repeat(decimals - parts[1].length);
+    if (parts[0].match(/^0+$/)) {
+      return parts[1] + '0'.repeat(decimals - parts[1].length);
+    } else {
+      return parts[0] + parts[1] + '0'.repeat(decimals - parts[1].length);
+    }
   } else {
     throw new Error('Invalid amount');
   }
